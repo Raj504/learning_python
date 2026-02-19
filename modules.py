ecbcdd081ce34b420a6596fd8f11ede586aa8a06
rajclassdev <raj@classcardapp.com>
2026-02-19 23:12:22 +0530

    Add modules.py for mathematical, random, time, and OS operations; add strings.py for string manipulation examples

---
+++ modules.py
@@ -0,0 +1,27 @@
+import math
+math.pi
+math.e
+math.factorial(5)
+math.ceil(6.3)
+math.floor(6.9)
+
+import random 
+random.randint(1, 100)
+
+a = [1, 2, 3, 4, 5]
+random.shuffle(a)
+a
+
+import time
+
+time.time()
+time.ctime()
+
+print("raj")
+time.sleep(2)
+print("Aryan")
+
+import os
+
+os.getcwd()
+os.listdir()
---
+++ strings.py
@@ -0,0 +1,7 @@
+c = "hello"
+print(c[0])
+
+
+a = "hello world"
+print(a[0:4])
+print(a[2:])
